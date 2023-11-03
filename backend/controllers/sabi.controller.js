1e749d431a62aecd9578789cc2c0b3fc8029668c
unknown <Public@NOBANDIG178.bi-central.local>
2023-11-03 02:25:31 -0600

    Add Consultar en ChatGPT con Contexto y Registrar Chat

--- backend/controllers/sabi.controller.js
+++ backend/controllers/sabi.controller.js
@@ -71,6 +71,65 @@ sabiController.consultarYRegistrarChat = async (req, res) => {
     }
 };
 
+sabiController.consultarYRegistrarChatConContexto = async (req, res) => {
+    try {
+        const nicknameChatNuevo = req.body.nickname;
+        const inputChatNuevo = req.body.content;
+        if(nicknameChatNuevo!="user"){ // Con Contexto
+            // Obtener todos los chats anteriores para tener un contexto
+            const documents = await chatModel.find({ nickname: nicknameChatNuevo });
+            //Añadir Contexto
+            const contexto = "MENSAJE:" + inputChatNuevo + "\n\n"
+            + "Acontinuación te envío todo un objeto JSON con chats que hemos tenido anteriormente. "
+            + "Donde input es el mensaje que yo te envie, el output es la respuesta que tu me diste, "
+            + "el nickname es mi nickname, el tema es sobre la categoría de la conversación a grandes rasgos, "
+            + "y la fechayhora es la fecha y hora en la que se realizo la conversación.\n" 
+            + "Utilizalo para obtener información y tener un contexto y "
+            + "así poder darme una respuesta más especifica al MENSAJE que te di anteriormente. "
+            + "Pero dentro de tus respuestas no me menciones que estas utilizando este contexto que yo te proporcione.\n\n"
+            + "MI NICKNAME ES: " + nicknameChatNuevo + "\n\n"
+            + "MI CONTEXTO ES: \n" + documents.toString();
+            const data = {
+                role: 'user',
+                content: contexto
+            };
+            axios.post('https://basicgptapi.azurewebsites.net/message/user', data)
+            .then(async response => { // Hacer la función de callback asíncrona
+                const outputChatNuevo = response.data.response.content;
+                const nicknameChatNuevo = req.body.nickname;
+                const temaChatNuevo = req.body.tema;
+                const fechaYHoraChatNuevo = (new Date()).toString()
+                try {
+                    const newChat = new chatModel({
+                        input: inputChatNuevo,
+                        output: outputChatNuevo,
+                        nickname: nicknameChatNuevo,
+                        tema: temaChatNuevo,
+                        fechayhora: fechaYHoraChatNuevo
+                    })
+                    const savedChat = await newChat.save()
+                    res.status(200).json({ 
+                        "message": "Chat registrado",
+                        "response": savedChat.output
+                    });
+                } catch (error) {
+                    console.log(error);
+                    res.status(500).json({message: 'Hubo un error'})
+                }
+            })
+            .catch(error => {
+                res.status(503).json(error)
+                console.error('Error posting data:', error);
+            });
+        } else { // Sin Contexto
+            await sabiController.consultarYRegistrarChat(req, res);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'Hubo un error'})
+    }  
+};
+
 sabiController.consultarChats = async (req, res) => {      
     try {
         const documents = await chatModel.find({ nickname: req.body.nickname });
--- backend/routes/sabi.routes.js
+++ backend/routes/sabi.routes.js
@@ -9,6 +9,8 @@ router.post('/consulta',api.consultar); // { "role": "user", "content": "Hola" }
 
 router.post('/consultaryregistrarchat',api.consultarYRegistrarChat); // { "content": "hola", "nickname": "Patito", "tema": "Salud", "fechaYHora": "02-11-2023 11:42" }
 
+router.post('/consultaryregistrarchatconcontexto',api.consultarYRegistrarChatConContexto); // { "content": "hola", "nickname": "Patito", "tema": "Salud", "fechaYHora": "02-11-2023 11:42" }
+
 router.post('/consultarchats',api.consultarChats); // { "nickname": "Patito" }
 
 module.exports = router;
